42450929045a9004ab643bf4f0e463028a06880a
nemo1064 <ankit.kr1064@gmail.com>
2020-02-25 13:02:08 +0530

    multiple working

--- src/app/demos/multiple/multiple.component.ts
+++ src/app/demos/multiple/multiple.component.ts
@@ -1,41 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Select2OptionData } from 'ngSelect2';
-import { Options } from 'select2';
+import { Component, OnInit } from "@angular/core";
+import { Select2OptionData } from "ngSelect2";
+import { Options } from "select2";
 
 @Component({
-  selector: 'app-multiple',
-  templateUrl: './multiple.component.html',
-  styleUrls: ['./multiple.component.css']
+  selector: "app-multiple",
+  templateUrl: "./multiple.component.html",
+  styleUrls: ["./multiple.component.css"]
 })
 export class MultipleComponent implements OnInit {
-  public exampleData: Array<Select2OptionData>;
+  public productData: Array<Select2OptionData>;
   public options: Options;
   public value: string[];
 
   ngOnInit() {
-    this.exampleData = [
+    this.productData = [
       {
-        id: 'multiple1',
-        text: 'Multiple 1'
+        id: "Product1",
+        text: "Product 1"
       },
       {
-        id: 'multiple2',
-        text: 'Multiple 2'
+        id: "product2",
+        text: "Product 2"
       },
       {
-        id: 'multiple3',
-        text: 'Multiple 3'
+        id: "product3",
+        text: "Product 3"
       },
       {
-        id: 'multiple4',
-        text: 'Multiple 4'
+        id: "product4",
+        text: "Product 4"
       }
     ];
 
-    this.value = ['multiple2', 'multiple4'];
+    this.value = [];
 
     this.options = {
-      width: '300',
+      width: "300",
       multiple: true,
       tags: true
     };
